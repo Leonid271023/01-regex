6de39bb732ab69328ee258c18ac9dab9fc79d087
leonid <lashurov44@gmail.com>
2025-11-10 06:22:30 +0200

    test 1 + jest

--- service/validate.js
+++ service/validate.js
@@ -1,6 +1,13 @@
-const validateEcmoscript = text =>{
+const validateEcmascriptText = text => {
     const pattern = /^ *ecmascript([1-9]|1[0-6])?$/i;
     return pattern.test(text);
 }
 
-module.exports = validateEcmoscript;
+
+const validateDomainZone = text => {
+    const pattern = /^(\w|[.-])+\.(com|org|il)$/i;
+    return pattern.test(text);
+}
+
+
+module.exports = {validateEcmascriptText, validateDomainZone}
--- service/validate.test.js
+++ service/validate.test.js
@@ -1,10 +1,41 @@
-const validateEcmoscriptText = require('./validate');
+const {validateEcmascriptText, validateDomainZone} = require('./validate');
+const {describe, test, expect} = require('@jest/globals');
 
 
-test('should return true if ECMAscript is provided', () => {
-    expect(validateEcmoscriptText('  ecmascript')).toBe(true);
-    expect(validateEcmoscriptText('ecmascript')).toBe(true);
-    expect(validateEcmoscriptText('  ecmascript6')).toBe(true);
-    expect(validateEcmoscriptText('  ecmascript16')).toBe(true);
-    expect(validateEcmoscriptText('  ECMAScript')).toBe(true);
-});
+describe('validateEcmascriptText', () => {
+    test('should return true if ECMAScript is provided', () => {
+        expect(validateEcmascriptText('   ecmascript')).toBe(true);
+        expect(validateEcmascriptText('ecmascript')).toBe(true);
+        expect(validateEcmascriptText('   ecmascript6')).toBe(true);
+        expect(validateEcmascriptText('   ecmascript16')).toBe(true);
+        expect(validateEcmascriptText('   ECMAScript')).toBe(true);
+    });
+
+
+    test('should return false if ECMAScript is not provided', () => {
+        expect(validateEcmascriptText('   ecmascrip')).toBe(false);
+        expect(validateEcmascriptText('ecmascript0')).toBe(false);
+        expect(validateEcmascriptText('   ecmascript19')).toBe(false);
+        expect(validateEcmascriptText('10   ecmascript16')).toBe(false);
+    });
+})
+
+
+describe('validateDomainZone', () => {
+    test('domain is valid', () => {
+        expect(validateDomainZone('abrakadabra.il')).toBe(true);
+        expect(validateDomainZone('abrakadabra.com')).toBe(true);
+        expect(validateDomainZone('abrakadabra.org')).toBe(true);
+        expect(validateDomainZone('abra-kadabra.il')).toBe(true);
+        expect(validateDomainZone('abra.kadabra2025.il')).toBe(true);
+        expect(validateDomainZone('abra_kadabra2025.il')).toBe(true);
+    });
+
+
+    test('domain is not valid', () => {
+        expect(validateDomainZone('abrakadabra.ru')).toBe(false);
+        expect(validateDomainZone('.com')).toBe(false);
+        expect(validateDomainZone('abrakadabraorg')).toBe(false);
+        expect(validateDomainZone('abra$kadabra.il')).toBe(false);
+    });
+})
